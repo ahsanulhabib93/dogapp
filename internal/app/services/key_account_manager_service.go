2917c25511eefa463787636ee46dde7082b13f05
Ramya KVR <ramya.kvr@shopf.co>
2021-11-25 13:13:53 +0530

    api changes

---
+++ internal/app/services/key_account_manager_service.go
@@ -0,0 +1,47 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	kampb "github.com/voonik/goConnect/api/go/ss2/key_account_manager"
+	"github.com/voonik/goFramework/pkg/database"
+	"github.com/voonik/ss2/internal/app/models"
+)
+
+type KeyAccountManagerService struct{}
+
+func (kams *KeyAccountManagerService) List(ctx context.Context, params *kampb.ListParams) (*kampb.ListResponse, error) {
+	resp := kampb.ListResponse{}
+	database.DBAPM(ctx).Model(&models.KeyAccountManager{}).Where("supplier_id = ?", params.GetSupplierId()).Scan(&resp.Data)
+	return &resp, nil
+}
+
+func (kams *KeyAccountManagerService) Add(ctx context.Context, params *kampb.KeyAccountManagerParam) (*kampb.BasicApiResponse, error) {
+	resp := kampb.BasicApiResponse{Success: false}
+
+	supplier := &models.Supplier{}
+	result := database.DBAPM(ctx).Model(&models.Supplier{}).First(supplier, params.GetSupplierId())
+	if result.RecordNotFound() {
+		resp.Message = "Supplier Not Found"
+	} else {
+		keyAccountManager := models.KeyAccountManager{
+			Supplier: *supplier,
+			Name:     params.GetName(),
+			Email:    params.GetEmail(),
+			Phone:    params.GetPhone(),
+		}
+		err := database.DBAPM(ctx).Model(&models.KeyAccountManager{}).Create(&keyAccountManager)
+
+		if err != nil && err.Error != nil {
+			errorMsg := fmt.Sprintf("Error while creating KeyAccountManager: %s", err.Error)
+			log.Println(errorMsg)
+			resp.Message = errorMsg
+		} else {
+			resp.Message = "KeyAccountManager Added Successfully"
+			resp.Success = true
+		}
+	}
+	return &resp, nil
+}
