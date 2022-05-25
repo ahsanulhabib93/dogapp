e428120a4863228409f34bc78bf0341070afa318
Osman Chowdhury <chowdhuryosman04@gmail.com>
2022-05-25 17:35:39 +0600

    feat(supplier_address_update): restrict supplier address update when supplier in verified/blocked state [MP-2373]

--- internal/app/services/supplier_address_service.go
+++ internal/app/services/supplier_address_service.go
@@ -72,7 +72,11 @@ func (sas *SupplierAddressService) Edit(ctx context.Context, params *addresspb.S
 	if result.RecordNotFound() {
 		resp.Message = "Supplier Address Not Found"
 	} else {
-		if supplierAddress.IsDefault && !params.GetIsDefault() {
+		supplier := models.Supplier{}
+		database.DBAPM(ctx).Model(&models.Supplier{}).First(&supplier, supplierAddress.SupplierID)
+		if !supplier.IsUpdateAllowed(ctx) {
+			resp.Message = "Update Not Allowed"
+		} else if supplierAddress.IsDefault && !params.GetIsDefault() {
 			resp.Message = "Default address is required"
 		} else {
 			err := database.DBAPM(ctx).Model(&supplierAddress).Updates(models.SupplierAddress{
