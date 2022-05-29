8c5eda36c0bdd63165d85fe2a3291989786d04a9
Osman Chowdhury <chowdhuryosman04@gmail.com>
2022-05-29 18:03:09 +0600

    feat(supplier_status): add test coverage for supplier varification rule [MP-2369]

--- internal/app/test/services/supplier_service/update_status_test.go
+++ internal/app/test/services/supplier_service/update_status_test.go
@@ -7,10 +7,12 @@ import (
 	. "github.com/onsi/gomega"
 
 	supplierpb "github.com/voonik/goConnect/api/go/ss2/supplier"
+	aaaModels "github.com/voonik/goFramework/pkg/aaa/models"
 	"github.com/voonik/goFramework/pkg/database"
 	test_utils "github.com/voonik/goFramework/pkg/unit_test_helper"
 	"github.com/voonik/ss2/internal/app/models"
 	"github.com/voonik/ss2/internal/app/services"
+	"github.com/voonik/ss2/internal/app/test/mocks"
 	"github.com/voonik/ss2/internal/app/test/test_helper"
 )
 
@@ -19,6 +21,7 @@ var _ = Describe("UpdateStatus", func() {
 
 	BeforeEach(func() {
 		test_utils.GetContext(&ctx)
+		aaaModels.CreateAppPreferenceServiceInterface()
 	})
 
 	Context("Update Supplier status", func() {
@@ -192,6 +195,67 @@ var _ = Describe("UpdateStatus", func() {
 			Expect(err).To(BeNil())
 			Expect(res.Success).To(Equal(false))
 			Expect(res.Message).To(Equal("Status transition not allowed"))
+		})
+	})
+
+	Context("Should return error", func() {
+		It("When no OTP verification or primary document given", func() {
+			supplier := test_helper.CreateSupplierWithAddress(ctx, &models.Supplier{
+				Status: models.SupplierStatusBlocked,
+			})
+			test_helper.CreatePaymentAccountDetail(ctx, &models.PaymentAccountDetail{SupplierID: supplier.ID, IsDefault: true})
+			param := &supplierpb.UpdateStatusParam{
+				Id:     supplier.ID,
+				Status: string(models.SupplierStatusVerified),
+			}
+			res, err := new(services.SupplierService).UpdateStatus(ctx, param)
+
+			Expect(err).To(BeNil())
+			Expect(res.Success).To(Equal(false))
+			Expect(res.Message).To(Equal("At least one primary document or OTP verification needed"))
+		})
+
+		It("When at least one primary document required for given supplier type", func() {
+			aaaModels.InjectMockAppPreferenceServiceInstance(mocks.GetAppPreferenceMock(map[string]interface{}{
+				"enabled_primary_doc_verification": []string{"Hlc"},
+			}))
+
+			isPhoneVerified := true
+			supplier := test_helper.CreateSupplierWithAddress(ctx, &models.Supplier{
+				IsPhoneVerified: &isPhoneVerified,
+				Status:          models.SupplierStatusBlocked,
+			})
+			test_helper.CreatePaymentAccountDetail(ctx, &models.PaymentAccountDetail{SupplierID: supplier.ID, IsDefault: true})
+			param := &supplierpb.UpdateStatusParam{
+				Id:     supplier.ID,
+				Status: string(models.SupplierStatusVerified),
+			}
+			res, err := new(services.SupplierService).UpdateStatus(ctx, param)
+
+			Expect(err).To(BeNil())
+			Expect(res.Success).To(Equal(false))
+			Expect(res.Message).To(Equal("At least one primary document required for supplier type: Hlc"))
+		})
+
+		It("When otp verification required for given supplier type", func() {
+			aaaModels.InjectMockAppPreferenceServiceInstance(mocks.GetAppPreferenceMock(map[string]interface{}{
+				"enabled_otp_verification": []string{"Hlc"},
+			}))
+
+			supplier := test_helper.CreateSupplierWithAddress(ctx, &models.Supplier{
+				Status:    models.SupplierStatusBlocked,
+				NidNumber: "1234567890",
+			})
+			test_helper.CreatePaymentAccountDetail(ctx, &models.PaymentAccountDetail{SupplierID: supplier.ID, IsDefault: true})
+			param := &supplierpb.UpdateStatusParam{
+				Id:     supplier.ID,
+				Status: string(models.SupplierStatusVerified),
+			}
+			res, err := new(services.SupplierService).UpdateStatus(ctx, param)
+
+			Expect(err).To(BeNil())
+			Expect(res.Success).To(Equal(false))
+			Expect(res.Message).To(Equal("OTP verification required for supplier type: Hlc"))
 		})
 	})
 })
