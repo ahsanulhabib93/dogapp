ecf5ce76a5f6dc233ee9c246bdc2c8923bddd9a3
Osman Chowdhury <chowdhuryosman04@gmail.com>
2022-05-29 17:59:11 +0600

    feat(mock): add app preference mock [MP-2369]

---
+++ internal/app/test/mocks/app_preference_mock.go
@@ -0,0 +1,18 @@
+package mocks
+
+import context "context"
+
+type mockAppPreference struct {
+	values map[string]interface{}
+}
+
+func GetAppPreferenceMock(value map[string]interface{}) mockAppPreference {
+	return mockAppPreference{value}
+}
+
+func (mock mockAppPreference) GetValue(ctx context.Context, key string, value interface{}) interface{} {
+	if v, f := mock.values[key]; f {
+		return v
+	}
+	return value
+}
