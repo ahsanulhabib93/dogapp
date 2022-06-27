819161ab0c1222233b56c0ea280765d131e328e9
Osman Chowdhury <chowdhuryosman04@gmail.com>
2022-06-28 03:47:27 +0600

    feat(worker): enabled worker for ss2 [MP-1555]

---
+++ cmd/worker/worker.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	goWorker "github.com/voonik/goFramework/pkg/worker"
+	"github.com/voonik/ss2/internal/app/helpers"
+	"github.com/voonik/work"
+)
+
+func main() {
+	goWorker.InitWorkerPool(10)
+	jobInstance := goWorker.GetJobInstance()
+	schedulerInstance := goWorker.GetSchedulerInstance()
+	opts := []goWorker.AccountOption{
+		goWorker.WithAccountandPortal(int64(1), int64(1)),
+		goWorker.WithAccountandPortal(int64(2), int64(2)),
+	}
+
+	jobInstance.RegisterJobWithOptions("change_pending_supplier_status", work.JobOptions{
+		MaxConcurrency: 0, Priority: 10, MaxFails: 1,
+	}, helpers.ChangePendingState, opts...)
+	schedulerInstance.SchedulePeriodicJob("@daily", "change_pending_supplier_status", map[string]interface{}{}, opts...)
+
+	goWorker.Start()
+}
