457bfcba872acafdc18cea0ee363a35243d2bdbb
Kazi Waseef <kazi.waseef@shopup.org>
2023-05-18 12:03:13 +0600

    feat(mock): removed unimplemented GetBool method for implemented method

--- internal/app/test/mocks/app_preference_mock.go
+++ internal/app/test/mocks/app_preference_mock.go
@@ -10,12 +10,6 @@ func GetAppPreferenceMock(value map[string]interface{}) mockAppPreference {
 	return mockAppPreference{value}
 }
 
-func (mock mockAppPreference) GetBool(ctx context.Context, key string) bool {
-	// TODO
-	// need to define the mock function which got added due to goFramework update
-	return true
-}
-
 func (mock mockAppPreference) GetValue(ctx context.Context, key string, value interface{}) interface{} {
 	if v, f := mock.values[key]; f {
 		return v
