f47365f2f6050561a1fe17c4c47cf8daaac92e53
Subash A <subash.annadurai@shopup.org>
2023-07-19 16:14:29 +0530

    Update stringers

--- internal/app/test/services/partner_service/partner_types_list_test.go
+++ internal/app/test/services/partner_service/partner_types_list_test.go
@@ -36,13 +36,13 @@ var _ = Describe("PartnerTypesList", func() {
 
 			transport := res.PartnerServiceTypeMappings[1]
 			Expect(transport.PartnerType).To(Equal("Transporter"))
-			Expect(len(transport.ServiceTypes)).To(Equal(9))
-			Expect(transport.ServiceTypes).To(Equal([]string{"L0", "L1", "L2", "L3", "Hlc", "Captive", "Driver", "CashVendor", "RedxHubVendor"}))
+			Expect(len(transport.ServiceTypes)).To(Equal(5))
+			Expect(transport.ServiceTypes).To(Equal([]string{"Captive", "Driver", "CashVendor", "RedxHubVendor", "CreditVendor"}))
 
 			rent := res.PartnerServiceTypeMappings[2]
 			Expect(rent.PartnerType).To(Equal("RentVendor"))
 			Expect(len(rent.ServiceTypes)).To(Equal(4))
-			Expect(rent.ServiceTypes).To(Equal([]string{"Hub Rent", "Warehouse Rent", "DBHouse Rent", "Office Rent"}))
+			Expect(rent.ServiceTypes).To(Equal([]string{"HubRent", "WarehouseRent", "DBHouseRent", "OfficeRent"}))
 		})
 	})
 
