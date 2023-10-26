3a1b3055fac4b6328f6be4b3b0157a2158d74c9d
Subash A <subash.annadurai@shopup.org>
2023-10-26 12:54:02 +0530

    Add bulk vendor onboard script

--- cmd/script/script.go
+++ cmd/script/script.go
@@ -14,6 +14,7 @@ func main() {
 	app := cli.NewApp()
 	app.Commands = []cli.Command{
 		updateSupplierType(),
+		addSuppliersInBulk(),
 	}
 	err := app.Run(os.Args)
 	if err != nil {
@@ -41,6 +42,30 @@ func updateSupplierType() cli.Command {
 			}
 			context := misc.SetInContextThreadObject(bgCtx, threadObject)
 			script.UpdateSupplierType(context)
+			return nil
+		},
+	}
+}
+func addSuppliersInBulk() cli.Command {
+	return cli.Command{
+		Name:  "addSuppliersInBulk",
+		Usage: "Add new suppliers in bulk",
+		Action: func(ctx *cli.Context) error {
+			bgCtx := context.Background()
+			threadObject := &misc.ThreadObject{
+				VaccountId:    12,
+				PortalId:      12,
+				CurrentActId:  12,
+				XForwardedFor: "5079327",
+				UserData: &misc.UserData{
+					UserId: 18,
+					Name:   "AFT2User",
+					Email:  "aft2user@gmail.com",
+					Phone:  "8801855533367",
+				},
+			}
+			context := misc.SetInContextThreadObject(bgCtx, threadObject)
+			script.AddSuppliersFromExcel(context)
 			return nil
 		},
 	}
