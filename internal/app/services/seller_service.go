c8166841288f4ff89955508b146d9b9672e83aec
Madhu-mitha-A <madhumitha.a@shopup.org>
2023-11-07 15:05:24 +0530

    add GetSellerByCondition api to seller service

--- internal/app/services/seller_service.go
+++ internal/app/services/seller_service.go
@@ -8,6 +8,7 @@ import (
 	"github.com/voonik/goFramework/pkg/database"
 
 	"github.com/voonik/ss2/internal/app/models"
+	"github.com/voonik/ss2/internal/app/utils"
 )
 
 type SellerService struct{}
@@ -25,5 +26,33 @@ func (ss *SellerService) GetByUserID(ctx context.Context, params *spb.GetByUserI
 		Seller: seller,
 	}
 
+	return &response, nil
+}
+
+func (SellerService) GetSellerByCondition(ctx context.Context, params *spb.GetSellerByConditionParams) (*spb.GetSellersResponse, error) {
+	response := spb.GetSellersResponse{}
+	sellers := []*spb.SellerObject{}
+	fields := params.GetFields()
+	condition := params.GetCondition()
+	if condition == "" {
+		log.Println("No condition specified")
+		response.Status = utils.Failure
+		response.Message = "no condition specified"
+		return &response, nil
+	}
+	query := database.DBAPM(ctx).Model(&models.Seller{}).Where(condition)
+	if fields != "" {
+		query = query.Select(fields)
+	}
+	query.Scan(&sellers)
+	if len(sellers) == 0 {
+		log.Println("Seller not found")
+		response.Status = utils.Failure
+		response.Message = "seller not found"
+		return &response, nil
+	}
+	response.Seller = sellers
+	response.Status = utils.Success
+	response.Message = "fetched seller details successfully"
 	return &response, nil
 }
