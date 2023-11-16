35a6c234cd9b1d07f9a813f477a4147cf6f2117e
Madhu-mitha-A <madhumitha.a@shopup.org>
2023-11-16 16:45:08 +0530

    add skeleton function for verify_address api

--- internal/app/services/vendor_address_service.go
+++ internal/app/services/vendor_address_service.go
@@ -11,3 +11,7 @@ type VendorAddressService struct{}
 func (vapb *VendorAddressService) GetData(ctx context.Context, params *vapb.GetDataParams) (*vapb.GetDataResponse, error) {
 	return nil, nil
 }
+
+func (vapb *VendorAddressService) VerifyAddress(ctx context.Context, params *vapb.VerifyAddressParams) (*vapb.BasicApiResponse, error) {
+	return nil, nil
+}
