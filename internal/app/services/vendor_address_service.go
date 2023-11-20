01260a75c7aab632720a9af78b52861a9479c3df
Madhu-mitha-A <madhumitha.a@shopup.org>
2023-11-20 09:07:20 +0530

    add get data api to vendor address

--- internal/app/services/vendor_address_service.go
+++ internal/app/services/vendor_address_service.go
@@ -13,61 +13,60 @@ import (
 
 type VendorAddressService struct{}
 
-// def get_data
-// 	params.permit!
-// 	seller_id = params[:seller_user_id] ? Seller.select("id").find_by(user_id: params[:seller_user_id]) : params[:seller_id]
-// 	vendor_addresses = VendorAddress.where(:seller_id => seller_id) if seller_id.present?
-// 	vendor_addresses = VendorAddress.unscoped.where(:id => params[:id]) if params[:id].present?
-// 	vendor_addresses = VendorAddress.where(:id => params[:scoped_id]) if params[:scoped_id].present?
-// 	vendor_addresses
-// end
-
-func (vapb *VendorAddressService) GetData(ctx context.Context, params *vapb.GetDataParams) (*vapb.GetDataResponse, error) {
-	response := vapb.GetDataResponse{Status: utils.Success}
+func (vas *VendorAddressService) GetData(ctx context.Context, params *vapb.GetDataParams) (*vapb.GetDataResponse, error) {
+	response := vapb.GetDataResponse{Status: utils.Failure}
 	userIds := params.GetUserIds()
 	sellerIds := params.GetSellerIds()
 	ids := params.GetIds()
 
 	if len(userIds) == 0 && len(sellerIds) == 0 && len(ids) == 0 {
-		response.Status = utils.Failure
 		response.Message = "param not specified"
 		return &response, nil
 	}
-
-	// query := database.DBAPM(ctx).Model(&models.Seller{}).Where("user_id = ?", userId)
 	query := database.DBAPM(ctx).Model(&models.VendorAddress{})
 	if len(ids) != 0 {
+		if len(sellerIds) != 0 || len(userIds) != 0 {
+			response.Message = fmt.Sprint("specify any one param")
+			return &response, nil
+		}
 		query = query.Where("id in (?)", ids)
-	}
-	if len(sellerIds) != 0 {
+	} else if len(sellerIds) != 0 {
+		if len(userIds) != 0 {
+			response.Message = fmt.Sprint("specify any one param")
+			return &response, nil
+		}
 		query = query.Where("seller_id in (?)", sellerIds)
-	}
-	if len(userIds) != 0 {
+	} else if len(userIds) != 0 {
 		sellers := []*models.Seller{}
 		err := database.DBAPM(ctx).Model(&models.Seller{}).Where("user_id in (?)", userIds).Scan(&sellers).Error
 		if err != nil {
-			logger.FromContext(ctx).Info("Error in vendor address service GetData API")
-			response.Status = utils.Failure
-			response.Message = fmt.Sprint("Error in vendor address service GetData API: ", err.Error())
+			logger.FromContext(ctx).Info("error in vendor address service GetData API")
+			response.Message = fmt.Sprint("error in vendor address service GetData API: ", err.Error())
 			return &response, nil
 		}
 		if len(sellers) == 0 {
 			response.Message = "seller not found with the user id"
 			return &response, nil
 		}
+		var sellerIDs []int
+		for _, seller := range sellers {
+			sellerIDs = append(sellerIDs, int(seller.ID))
+		}
+		query = query.Where("seller_id in (?)", sellerIDs)
 	}
-	vendorAddress := []*models.VendorAddress{}
-	err := query.Scan(vendorAddress).Error
+	vendorAddress := []*vapb.VendorAddressObject{}
+	err := query.Scan(&vendorAddress).Error
 	if err != nil {
-		logger.FromContext(ctx).Info("Error in vendor address service GetData API: ", err.Error())
-		response.Status = utils.Failure
-		response.Message = fmt.Sprint("Error in vendor address service GetData API: ", err.Error())
+		logger.FromContext(ctx).Info("error in vendor address service GetData API: ", err.Error())
+		response.Message = fmt.Sprint("error in vendor address service GetData API: ", err.Error())
 		return &response, nil
 	}
+	response.Status = utils.Success
 	if len(vendorAddress) == 0 {
 		response.Message = "vendor address not found"
 		return &response, nil
 	}
+	response.VendorAddress = vendorAddress
 	response.Message = "fetched vendor address successfully"
 	return &response, nil
 }
