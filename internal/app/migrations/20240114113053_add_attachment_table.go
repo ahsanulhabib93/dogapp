df960adea01966545801fdc9020b6abc67469d04
Subash A <subash.annadurai@shopup.org>
2024-01-15 14:59:53 +0530

    feat(attachment): Add migrations

---
+++ internal/app/migrations/20240114113053_add_attachment_table.go
@@ -0,0 +1,22 @@
+package migrations
+
+import (
+	"github.com/jinzhu/gorm"
+	migrator "github.com/voonik/goFramework/pkg/migrations"
+	"github.com/voonik/ss2/internal/app/models"
+	"gopkg.in/gormigrate.v1"
+)
+
+func init() {
+	migrator.Register(&gormigrate.Migration{
+		ID: "20240114113053",
+		Migrate: func(tx *gorm.DB) error {
+			return tx.AutoMigrate(
+				models.Attachment{},
+			).Error
+		},
+		Rollback: func(tx *gorm.DB) error {
+			return nil
+		},
+	})
+}
