a70be10d45ebce116d9cb2fc53fbbaa229d5c3ee
Md. Ahsanul Habib <82897817+ahsanulhabib93@users.noreply.github.com>
2024-09-18 01:56:38 +0600

    Delete internal directory

--- internal/app/models/query_strings.go
+++
@@ -1,5 +0,0 @@
-package models
-
-func SamSellerJoinString() string {
-	return "inner join sellers on sellers.id = seller_account_managers.seller_id and sellers.vaccount_id = seller_account_managers.vaccount_id"
-}
--- internal/app/services/errors.go
+++
@@ -1,7 +0,0 @@
-package services
-
-import "errors"
-
-var (
-	NotFound = errors.New("service: not found")
-)
