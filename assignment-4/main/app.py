324477fe8660f9eb64f44b353487ce6b8812ce7d
Md. Ahsanul Habib <82897817+ahsanulhabib93@users.noreply.github.com>
2024-09-18 01:52:58 +0600

    Delete assignment-4 directory

--- assignment-4/main/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_starterkit.main.config import create_app
-
-flask_app = create_app()
-
-
-@flask_app.route("/")
-def home_route():
-    return "GitHub hook is working!"
--- assignment-4/run.py
+++
@@ -1,9 +0,0 @@
-from flask_starterkit.main.app import flask_app
-from dotenv import load_dotenv
-
-if __name__ == "__main__":
-    load_dotenv(dotenv_path="./.env")
-    flask_app.run()
-
-
-#test
